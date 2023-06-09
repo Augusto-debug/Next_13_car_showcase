05cd3cc31fc0647eada41130879cc349d0003e29
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-06-09 18:16:24 -0300

    Fixed some bugs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,11 @@ import Image from "next/image"
 
 function Hero() {
   return (
-    <div className="hero font-extrabold">Hero</div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title"></h1>
+      </div>
+    </div>
   )
 }
 
