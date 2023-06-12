41d44141a65d9756fa4b8be130c83dcdea573f05
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-06-12 18:13:40 -0300

    updating the layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Footer = () => {
-  return;
-  <footer></footer>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 border-2 border-red-600">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10 border-2 border-green-600">
+        <div className="flex flex-col justify-start items-start gap-6 border-blue-600 border-4">
+          <Image src="logo.svg" alt="logo" width={118} height={18} />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
