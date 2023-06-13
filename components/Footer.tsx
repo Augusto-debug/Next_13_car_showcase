9c6f470f481465dce523fe9cf8f962ccbd193be2
Augusto-debug <augustocesarrezende64@gmail.com>
2023-06-13 14:18:24 -0300

    Almost finishing the footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
+import { footerLinks } from "@/constants";
+
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 border-2 border-red-600">
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10 border-2 border-green-600">
-        <div className="flex flex-col justify-start items-start gap-6 border-blue-600 border-4">
-          <Image src="logo.svg" alt="logo" width={118} height={18} />
+    <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-start gap-6'>
+          <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
+          <p className='text-base text-gray-700'>
+            Carhub 2023 <br />
+            All Rights Reserved &copy;
+          </p>
+        </div>
+
+        <div className="flex-1 w-full flex md: justify-end flex-wrap max-md:mt-10 gap-20">
+          {footerLinks.map((item) => (
+            <div key={item.title} className="flex flex-col gap-6 text-base min-w-[170px]">
+              <h3 className="font-bold">{item.title}</h3>
+              <div className="flex flex-col gap-5  max-w-3xl">
+                {item.links.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.url}
+                    className="text-gray-500"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </footer>
+    </footer >
   );
 };
 
