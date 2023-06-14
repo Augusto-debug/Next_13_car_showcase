8eb53eb334e96fd53ff599cb202e089fb0b2f9c5
Augusto-debug <augustocesarrezende64@gmail.com>
2023-06-14 16:51:12 -0300

    Fixed the image overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,11 +30,10 @@ function Hero() {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png " fill className="object-contain" alt="hero" />
-
-          <div className="hero__image-overlay"></div>
+        </div>
+          <div className="hero__image-overlay" />
         </div>
       </div>
-    </div>
   );
 }
 
