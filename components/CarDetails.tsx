dd1ddf4fc72b49059cd455cd38474f9c8548e8df
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-06-26 18:35:25 -0300

    Working on modal component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { CarProps } from "@/types";
 import Image from "next/image";
-import React from "react";
+import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
 interface CarDetailsProps {
@@ -10,7 +11,51 @@ interface CarDetailsProps {
 }
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
-  return <div> </div>;
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25">Hi there</div>
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform roundex-2xl bg-white">
+                  <button type="button" onClick={closeModal}>
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default CarDetails;
