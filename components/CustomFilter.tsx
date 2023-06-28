3b4917c11b1e3275f5cfe337e8b3c5f8f53effa6
Augusto Cesar <augustocesarrezende64@gmail.com>
2023-06-28 19:38:14 -0300

    Working on Custom Filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, Fragment } from "react";
+import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Listbox, Transition } from "@headlessui/react";
@@ -9,7 +9,12 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+        }}
+      >
         <div className="relative w-fit z-10 ">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
@@ -21,6 +26,14 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
               className="ml-4 object-contain"
             />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options"></Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
